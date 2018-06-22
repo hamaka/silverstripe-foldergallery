ebc501c69d7258a0f715a6b8c6e39b6ff261d418
Juanitou <info@juanmolina.eu>
2018-06-22 12:44:13 +0200

    Remove subfolder requirement in _config.php

--- _config.php
+++ _config.php
@@ -3,31 +3,15 @@
  * A lightweight folder based gallery module for the CMS SilverStripe
  *
  * Contains global settings for the SilverStripe CMS foldergallery module.
- * 
+ *
  * LICENSE: GNU General Public License 3.0
- * 
- * @platform    CMS SilverStripe 3.1.x (or higher)
- * @package     cwsoft-foldergallery
- * @version     2.7.3
+ *
+ * @platform    CMS SilverStripe 4 (or higher)
+ * @package     juanitou-foldergallery
+ * @version     4.0
  * @author      cwsoft (http://cwsoft.de)
+ * @author      Juanitou (http://juanmolina.eu)
  * @copyright   cwsoft
+ * @copyright   Juanitou
  * @license     http://www.gnu.org/licenses/gpl-3.0.html
 */
-
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// DO NOT CHANGE ANYTHING BELOW THIS LINE UNLESS YOU KNOW WHAT YOU ARE DOING :-)
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// ensure module is stored in folder "cwsoft-foldergallery"
-$moduleName = 'cwsoft-foldergallery';
-$folderName = basename(dirname(__FILE__));
-
-if ($folderName != $moduleName) {
-	user_error(
-		_t(
-			'_config.WRONG_MODULE_FOLDER', 
-			'Please rename the folder "{folderName}" into "{moduleName}" to get the {moduleName} module working properly.',
-			array('moduleName' => $moduleName, 'folderName' => $folderName)
-		),
-		E_USER_ERROR
-	);
-}
